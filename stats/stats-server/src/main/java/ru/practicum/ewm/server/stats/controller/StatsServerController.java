7cc0d2b305f67def201c19baf83b05c1ccc1bfeb
Andrey Zhirnov <deicida6@yandex.ru>
2024-08-23 10:04:52 +0300

    fix after review

--- stats/stats-server/src/main/java/ru/practicum/ewm/server/stats/controller/StatsServerController.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/server/stats/controller/StatsServerController.java
@@ -2,16 +2,14 @@ package ru.practicum.ewm.server.stats.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.dto.stats.EndpointHitDto;
 import ru.practicum.ewm.dto.stats.ViewStats;
 import ru.practicum.ewm.server.stats.service.StatsService;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Slf4j
@@ -20,7 +18,6 @@ import java.util.List;
 public class StatsServerController {
 
     private final StatsService statsService;
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @PostMapping("/hit")
     public EndpointHitDto addUser(
@@ -31,16 +28,12 @@ public class StatsServerController {
 
     @GetMapping("/stats")
     public List<ViewStats> getStats(
-            @RequestParam(name = "start") String startString,
-            @RequestParam(name = "end") String endString,
+            @RequestParam(name = "start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss", iso = DateTimeFormat.ISO.DATE) LocalDateTime start,
+            @RequestParam(name = "end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss", iso = DateTimeFormat.ISO.DATE) LocalDateTime end,
             @RequestParam(required = false, name = "uris") String[] uris,
             @RequestParam(name = "unique", defaultValue = "false") boolean unique) throws UnsupportedEncodingException {
 
         log.info("Get stats");
-
-        LocalDateTime start = LocalDateTime.parse(URLDecoder.decode(startString, StandardCharsets.UTF_8), formatter);
-        LocalDateTime end = LocalDateTime.parse(URLDecoder.decode(endString, StandardCharsets.UTF_8), formatter);
-
         return statsService.getStats(start, end, uris, unique);
     }
 
